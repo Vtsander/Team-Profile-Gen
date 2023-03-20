8a09e5c11f5b5af9416ebe660004b4a05610806d
Veroneca Sander <nguyenveroneca@yahoo.com>
2023-03-19 17:01:28 -0700

    removing code to start over

--- Develop/reset.js
+++
@@ -1,8 +0,0 @@
-// Removes the team.html file from the dist/ folder.
-// Used in `npm run reset` script
-
-const fs = require('fs');
-if (fs.existsSync('./dist/fullteam.html')) {
-  fs.unlinkSync('./dist/fullteam.html');
-  console.log('/dist/ folder reset!');
-}
